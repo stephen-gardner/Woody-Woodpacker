7703b9a788e3107502f0f0f08f6911f7540538f1
Alexandr Sarandi <sarandi.alex@gmail.com>
2019-06-16 18:52:05 -0700

    added asm code to display woody string

--- decryptor.s
+++ decryptor.s
@@ -1,28 +1,41 @@
 bits 64
 default rel
 global _start
+
 section .text
 _start:
+    pushf
     push    rdi
     push    rsi
     push    rdx
+    push    rax
+
+    mov     rax, 1
+    mov     rdi, 1
+    lea     rsi, [rel msg]
+    mov     rdx, ep-msg
+    syscall
+
     mov     rdi, [rel address]
     mov     rsi, [rel size]    
     mov     rdx, [rel key]
     add     rsi, rdi
-.loop:    
-    cmp     rdi, rsi
-    jge     .done
+
+.a: cmp     rdi, rsi
+    jge     .b
     xor     byte [rdi], dl
     inc     rdi
     ror     rdx, 8
-    jmp     .loop
-.done:
+    jmp     .a
+
+.b: pop     rax
     pop     rdx
     pop     rsi
     pop     rdi
+    popf
     jmp     [rel ep]
 
+    msg     db ".....WOODY.....",10
     ep      dq 0x1111111122222222
     address dq 0x3333333344444444
     size    dq 0x5555555566666666
